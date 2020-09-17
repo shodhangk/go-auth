b2f52190f05d6eccc2899ef99e00949b83fa713b
Shodhan <shodhan.gk@robosoftin.com>
2020-09-17 08:44:14 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/shodhangk/go-auth
+
+go 1.14
+
+require (
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
+)
---
+++ utils/utils.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"././models"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql" //Gorm postgres dialect interface
+	"github.com/joho/godotenv"
+)
+
+//ConnectDB function: Make database connection
+func ConnectDB() *gorm.DB {
+
+	//Load environmenatal variables
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	username := os.Getenv("databaseUser")
+	password := os.Getenv("databasePassword")
+	databaseName := os.Getenv("databaseName")
+	databaseHost := os.Getenv("databaseHost")
+
+	//Define DB connection string
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", databaseHost, username, databaseName, password)
+
+	//connect to db URI
+	db, err := gorm.Open("mysql", dbURI)
+
+	if err != nil {
+		fmt.Println("error", err)
+		panic(err)
+	}
+	// close db when not in use
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(
+		&models.User{})
+
+	fmt.Println("Successfully connected!", db)
+	return db
+}
